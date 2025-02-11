1b92a8230cdace9f6a6dc668881cfe100e9d9da6
Seif <seifsayed121@gmail.com>
2025-02-11 17:24:47 +0200

    Projects Section Complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Dawnload CV 
+// Download CV 
 const pdfUrl = "/assets/Seif_El_Sayed CV.pdf"
 const cvBtn = document.getElementById("cv-btn");
 cvBtn.addEventListener("click", () => {
@@ -13,9 +13,21 @@ cvBtn.addEventListener("click", () => {
 const skills = document.getElementById("skills")
 let spans = document.querySelectorAll(".skills .progress span")
 window.onscroll = () => {
-    if (window.scrollY >= skills.offsetTop - window.innerHeight + 100) {
+    if (window.scrollY >= skills.offsetTop - 230) {
         spans.forEach((span) => {
             span.style.width = span.dataset.width;
         });
     }
 }
+// Reveal on Scroll
+document.addEventListener("DOMContentLoaded", () => {
+    const projects = document.querySelectorAll(".projects .project");
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.2 });
+    projects.forEach(project => observer.observe(project));
+});
