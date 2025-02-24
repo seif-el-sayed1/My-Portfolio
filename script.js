054e5a564afcced0729efeeaf25739d4a3a94dfa
Seif <seifsayed121@gmail.com>
2025-02-24 22:40:48 +0200

    cv update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Download CV 
-const pdfUrl = "/assets/Seif_El_Sayed Frontend developer.pdf";
+const pdfUrl = "/assets/Seif_El_Sayed MERN Stack developer.pdf";
 const cvBtn = document.getElementById("cv-btn");
 cvBtn.addEventListener("click", () => {
     const link = document.createElement("a"); 
     link.href = pdfUrl; 
-    link.download = "Seif_El_Sayed Frontend developer"; 
+    link.download = "Seif_El_Sayed CV"; 
     document.body.appendChild(link); 
     link.click();
     document.body.removeChild(link); 
